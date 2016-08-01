8ca9502386ee8b04d2c0bf187ef258c1f76bb0e6
Chad Rehm KineticData <chad.rehm@kineticdata.com>
2016-08-01 12:02:00 -0500

    added comment to search.js

--- js/search.js
+++ js/search.js
@@ -1,4 +1,7 @@
 (function(){
+  /* On load of the search.jsp page bring focus back to the search input and add a space to the end of the text.
+   * This will allow the user to add addtional search terms to the query for a more specific search.
+   */
   $(function(){
     $('#sidebar-search input:nth-child(2)').focus().val($('#sidebar-search input:nth-child(2)').val()+" ");
   });
